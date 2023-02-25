b2444a675fb65c33971df53f148face897cf2135
honeybunny002 <104381813+honeybunny002@users.noreply.github.com>
2023-02-25 14:21:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config(object):
     APP_ID =27307346 
     API_HASH = "ab2ff146d9e34248742343824e5976ac"
     TG_USER_SESSION = "BQGgrVIAGVj4xUfN5t2bj4sLKLFjjomIr_nB643RF_Ws2TGAxdDonYlmLNxL5fHjVjapDniJ0wgMhYjiSzmv7m1gVNJpIwWcryEI3Ci6uA8vLZvsOwiFy0HGlBx_0tUU4F3l2HD8V5i53GHXq1fjkuNrbW60Us7A424S10O5fYmuIxy7fMjs1-oEif3lwgSb-Jjp3uYdLuBwqCCzHKLJQKu9cmTWyS55WXo3QtodHv5vZJPvNjWbOhhivuVr-JaIkdM8U730HYHC1BFXBBpIKQhUMaNPw_tCjONXkqHk1dlB-ESbzDJJsXQhyyJ_kCN_LCg4XsMZDgNJejXL-OX2ndL26ENh1QAAAAFpcMmuAA"
-    DB_URI = "postgres://root:root@localhost:5432/adramadb"
+    DB_URI = "postgres://root:root@192.46.227.231:5432/adramadb"
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
